7d14b4b387fd8384546d3803d7f182ef1a73eb27
manishhub9 <myadav@hub9.io>
2019-03-30 08:59:15 +0530

    Saerch by tags and tags app.

--- first_app/first_app/settings.py
+++ first_app/first_app/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'products',
     'search',
+    'tags',
+    'carts',
 ]
 
 MIDDLEWARE = [
--- first_app/products/models.py
+++ first_app/products/models.py
@@ -21,7 +21,9 @@ class ProductQuerySet(models.query.QuerySet):
 	def featured(self):
 		return self.filter(featured=True, active=True)
 	def search(self,query):
-		lookups = Q(title__icontains=query) | Q(description__icontains=query)
+		lookups = (Q(title__icontains=query) |
+					Q(description__icontains=query) |
+					Q(tag__title__icontains=query))
 		return self.filter(lookups).distinct()
 
 class ProductManager(models.Manager):
