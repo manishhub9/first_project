7be32aab64fb1718a38dec109fee19d15e5864cd
manishhub9 <myadav@hub9.io>
2019-04-03 23:26:09 +0530

    Cart model changes.

--- first_app/carts/models.py
+++ first_app/carts/models.py
@@ -30,6 +30,7 @@ class Cart(models.Model):
     user = models.ForeignKey(User,null=True,blank=True)
     products = models.ManyToManyField(Product, blank=True)
     total = models.DecimalField(default=0.00,max_digits=100,decimal_places=2)
+    subtotal = models.DecimalField(default=0.00,max_digits=100,decimal_places=2)
     updated = models.DateTimeField(auto_now=True)
     timestamp = models.DateTimeField(auto_now_add=True)
     objects = CartManager()
