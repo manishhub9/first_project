cdbcde13e1e777b2d0115fbcd8ce17ace4b60206
manishhub9 <myadav@hub9.io>
2019-04-05 11:54:19 +0530

    Cart add/update changes

--- first_app/carts/views.py
+++ first_app/carts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .models import Cart
 from products.models import Product
 
+
 def cart_home(request):
     cart_obj, new_obj = Cart.objects.new_or_get(request)
     return render(request,'carts/home.html',{})
@@ -9,5 +10,8 @@ def cart_update(request):
     produtc_id = 7
     product_obj = Product.objects.get(id=produtc_id)
     cart_obj, new_obj = Cart.objects.new_or_get(request)
-    cart_obj.products.add(product_obj)
+    if product_obj in cart_obj.products.all():
+        cart_obj.products.remove(product_obj)
+    else:
+        cart_obj.products.add(product_obj)
     return redirect("cart:home")
--- first_app/products/views.py
+++ first_app/products/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView
 from .models import Product
+from carts.models import Cart
 from django.http import Http404
 
 class ProductListView(ListView):
@@ -10,3 +11,8 @@ class ProductListView(ListView):
 class ProductDetailSlugView(DetailView):
 	queryset = Product.objects.all()
 	template_name = 'products/detail.html'
+	def get_context_data(self,*args,**kwargs):
+		context = super(ProductDetailSlugView, self).get_context_data(*args, **kwargs)
+		cart_obj,new_obj = Cart.objects.new_or_get(self.request)
+		context['cart'] = cart_obj
+		return context
