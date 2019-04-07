b684ac278112f2e47d92536076033e91cb551513
manishhub9 <myadav@hub9.io>
2019-04-08 01:04:11 +0530

    Cart update, Product model changes.

--- first_app/carts/views.py
+++ first_app/carts/views.py
@@ -5,14 +5,19 @@ from products.models import Product
 
 def cart_home(request):
     cart_obj, new_obj = Cart.objects.new_or_get(request)
-    return render(request,'carts/home.html',{})
+    return render(request,'carts/home.html',{'cart':cart_obj})
+
 def cart_update(request):
-    print(request.POST)
-    produtc_id = 7
-    product_obj = Product.objects.get(id=produtc_id)
-    cart_obj, new_obj = Cart.objects.new_or_get(request)
-    if product_obj in cart_obj.products.all():
-        cart_obj.products.remove(product_obj)
-    else:
-        cart_obj.products.add(product_obj)
+    product_id = request.POST.get('product_id')
+    if product_id is not None:
+        try:
+            product_obj = Product.objects.get(id=product_id)
+        except Product.DoesNotExist:
+            print("product not found.")
+            return redirect("cart:home")
+        cart_obj, new_obj = Cart.objects.new_or_get(request)
+        if product_obj in cart_obj.products.all():
+            cart_obj.products.remove(product_obj)
+        else:
+            cart_obj.products.add(product_obj)
     return redirect("cart:home")
