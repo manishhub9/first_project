c01f4ab23e9c9c2660b6e1dcce3dc8fde1ce4724
manishhub9 <myadav@hub9.io>
2019-04-11 14:50:25 +0530

    Order and Cart signals update.

--- first_app/first_app/utils.py
+++ first_app/first_app/utils.py
@@ -9,9 +9,9 @@ def random_string_generator(size=10, chars=string.ascii_lowercase + string.digit
 def unique_order_id_generartor(instance):
     order_new_id = random_string_generator()
     Klass = instance.__class__
-    qs_exists = Klass.object.filter(order_id = order_new_id)
-    if qs.exists:
-        return unique_order_id_generartor(instance)
+    qs_exists = Klass.objects.filter(order_id = order_new_id).exists()
+    if qs_exists:
+        return unique_slug_generator(instance)
     return order_new_id
 def unique_slug_generator(instance, new_slug=None):
 
--- first_app/orders/models.py
+++ first_app/orders/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from carts.models import Cart
 from first_app.utils import unique_order_id_generartor
-from django.db.models.signals import pre_save
+from django.db.models.signals import pre_save, post_save
 
 ORDER_STATUS_CHOICES = (
             ('created','Created'),
@@ -17,9 +17,28 @@ class Order(models.Model):
     total = models.DecimalField(default=0.00,max_digits=100,decimal_places=2)
     def __str__(self):
         return self.order_id
+    def update_total(self):
+        cart_total = self.cart.total
+        shipping_total = self.shipping_total
+        new_total = cart_total + shipping_total
+        self.total = new_total
+        self.save()
+        return new_total
 
 def pre_save_create_order_id(sender, instance,*args,**kwargs):
     if not instance.order_id:
         instance.order_id = unique_order_id_generartor(instance)
-
 pre_save.connect(pre_save_create_order_id,sender=Order)
+def post_save_cart_total(sender,instance,created,*args,**kwargs):
+    if not created:
+        cart_obj = instance
+        qs = Order.objects.filter(cart__id=cart_obj.id)
+        if qs.count() == 1:
+            order_obj = qs.first()
+            order_obj.update_total()
+post_save.connect(post_save_cart_total, sender=Cart)
+
+def post_save_order(sender, instance, created, *args, **kwargs):
+    if created:
+        instance.update_total()
+post_save.connect(post_save_order, sender=Order)
