a22d91b267566d748930a76e69d258c2fdbeae73
manishhub9 <myadav@hub9.io>
2019-04-16 09:11:02 +0530

    order checkout changes.

--- first_app/orders/models.py
+++ first_app/orders/models.py
@@ -3,6 +3,7 @@ from carts.models import Cart
 from first_app.utils import unique_order_id_generartor
 from django.db.models.signals import pre_save, post_save
 import math
+from billing.models import BillingProfile
 
 ORDER_STATUS_CHOICES = (
             ('created','Created'),
@@ -12,10 +13,12 @@ ORDER_STATUS_CHOICES = (
             )
 class Order(models.Model):
     order_id = models.CharField(max_length=120, blank=True)
+    billing_profile = models.ForeignKey(BillingProfile, null=True,blank=True)
     cart = models.ForeignKey(Cart)
     status = models.CharField(max_length=120, default='created',choices=ORDER_STATUS_CHOICES)
     shipping_total = models.DecimalField(default=5.99,max_digits=100,decimal_places=2)
     total = models.DecimalField(default=0.00,max_digits=100,decimal_places=2)
+    active = models.BooleanField(default=True)
     def __str__(self):
         return self.order_id
     def update_total(self):
